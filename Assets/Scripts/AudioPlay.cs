ee960a1326b896e057c02f557a97b4374409eb9e
Violente <violentemertv@gmail.com>
2016-11-24 20:43:24 +0300

    refresh sound

---
+++ Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlay : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<AudioSource>().Play(); //Play Explosion Sound
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
